3cde6c6033761d7d086bf7e3e07217dce8c89b51
sunibnesazzad <sunibne.sazzad@gmail.com>
2023-07-02 00:16:13 +0600

    Completing the Recharts Grid, Bar chart, and pie chart part

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import DashboardStatsGrid from './shared/DashboardStatsGrid.jsx';
+import  TransactionChart  from './shared/TransactionChart.jsx';
+import BuyerProfilePieChart from './shared/BuyerProfilePieChart.jsx';
 
 export const Dashboard = () => {
   return (
-    <div> 
-        <p>This is Dashboard</p>
-        <Link to="/products" className='underline'>Go to Product Page</Link>
+    <div className='flex flex-col gap-4'> 
+        <DashboardStatsGrid />
+        <div className="flex flex-row gap-4 w-full">
+				    <TransactionChart />
+				    <BuyerProfilePieChart />
+			  </div>
     </div>
   )
 }
