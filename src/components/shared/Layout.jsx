fcfaecec1ea0a7c2c910f93ab90c9d7f53e282ba
sunibnesazzad <sunibne.sazzad@gmail.com>
2023-07-01 22:05:25 +0600

    Completing the Navbar part and read the Sazzad Readme file

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './sidebar.jsx';
+import Header from './header.jsx';
 
 export default function Layout() {
   return (
-    <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
+    <div className="bg-neutral-100 h-screen w-screen overflow-hidden flex flex-row">
       <div>
         <Sidebar/>
       </div>
-      <div className='p-4'>
-       <div className='bg-teal-200'>Header</div>
-       <div>{<Outlet />}</div>
-      </div>
+              {/* Nav bar code */}
+
+        <div className="flex flex-col flex-1">
+
+				<Header />
+
+				<div className="flex-1 p-4 min-h-0 overflow-auto">
+					<Outlet />
+          
+				</div>
+			</div>
+
     </div>
   )
 }
